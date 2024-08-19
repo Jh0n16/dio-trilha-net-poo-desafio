870db2583f2eb0773203743c4248aea50a17e9e1
Jh0n16 <57267877+Jh0n16@users.noreply.github.com>
2024-08-19 13:48:11 -0300

    feat: implementando o comportamento do projeto no arquivo Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("9999-9999", "Nokia 1", "123456789", 128);
+
+Iphone iphone = new Iphone("8888-8888", "Iphone X", "987654321", 64);
+
+nokia.InstalarAplicativo("YouTube");
+nokia.Ligar();
+
+Console.WriteLine("----------------------");
+
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
